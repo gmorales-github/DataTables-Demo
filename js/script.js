276fd49af21cfaec9f02bc76fd4a5c0f85fc1de3
Gustavo Morales <gustavom.morales@gmail.com>
2021-12-10 09:35:46 -0300

    Versión con AJAX y fuente de datos con un archivo externo json
    Versión con AJAX y fuente de datos con un archivo externo json.

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
-    $.ajax({
-        url: 'https://randomuser.me/api/',
-        dataType: 'json',
-        success: function(data) {
-            console.log(data);
-        }
-    });
-
     $('#Jtabla').DataTable({
-        "scrollX": false
+        "scrollX": false,
+        "ajax": "data/objects.txt",
+        "columns": [
+            { "data": "name" },
+            { "data": "position" },
+            { "data": "office" },
+            { "data": "extn" },
+            { "data": "start_date" },
+            { "data": "salary" }
+        ]
     });
-
 });
