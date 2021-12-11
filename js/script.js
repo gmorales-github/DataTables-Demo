cc09aeb968b68bd720d2deced7febca61995d0b7
Gustavo Morales <gustavom.morales@gmail.com>
2021-12-11 14:12:36 -0300

    Se agrega el archivo response.json

--- js/script.js
+++ js/script.js
@@ -1,14 +1,22 @@
 $(document).ready(function() {
     $('#Jtabla').DataTable({
-        "scrollX": false,
-        "ajax": "data/objects.txt",
+        "scrollX": true,
+        ajax: {
+            url: 'data/response.json',
+            dataSrc: ''
+        },
         "columns": [
-            { "data": "name" },
-            { "data": "position" },
-            { "data": "office" },
-            { "data": "extn" },
-            { "data": "start_date" },
-            { "data": "salary" }
+            { "data": "id_origen" },
+            { "data": "origen" },
+            { "data": "id_producteca" },
+            { "data": "id_sap" },
+            { "data": "fecha_obtencion" },
+            { "data": "nombre" },
+            { "data": "nick_name" },
+            { "data": "numero_dni" },
+            { "data": "materiales" },
+            { "data": "precio_total" },
+            { "data": "etiqueta" }
         ]
     });
 });
